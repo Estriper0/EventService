9a11b45add31a496df36fa05594a8a9dced41f07
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-12 19:41:29 +0300

    Улучшил код

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/Estriper0/EventService/internal/config"
+	"github.com/Estriper0/EventService/pkg/database"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
 )
 
 func main() {
 	config := config.New()
 
-	fmt.Println(config.DB)
-	db, err := sql.Open(
-		"postgres",
-		fmt.Sprintf(
-			"postgres://%s:%s@%s:%s/%s?sslmode=%s",
-			config.DB.DbUser,
-			config.DB.DbPassword,
-			config.DB.DbHost,
-			config.DB.DbPort,
-			config.DB.DbName,
-			config.DB.SSLMode,
-		),
-	)
-	if err != nil {
-		panic(err)
-	}
+	db := database.GetDB(&config.DB)
 	defer db.Close()
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
